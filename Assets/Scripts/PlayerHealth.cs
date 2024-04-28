2fe2b626d948cb65300dc1dad0c0021fd3700a5c
박형곤 <h.gon0530@gmail.com>
2024-04-28 21:31:32 +0900

    - 플레이어 데미지 효과(후처리) 추가 - 플레이어 데미지, 사망 사운드 추가

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,16 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 using UnityEngine.UI;
 
 public class PlayerHealth : LivingEntity
 {
     public Slider healthSlider;
     private Animator playerAnimator;
+    private AudioSource playerAudioPlayer;
+    public AudioClip playerHurtClip;
+    public AudioClip playerDeathClip;
+    public PostProcessVolume postProcessVolume;
+    public PostProcessProfile normalProfile;
+    public PostProcessProfile hurtProfile;
 
     private void Awake()
     {
         playerAnimator = GetComponent<Animator>();
+        playerAudioPlayer = GetComponent<AudioSource>();
     }
 
     private void Start()
@@ -26,7 +35,10 @@ public class PlayerHealth : LivingEntity
             return;
 
         base.OnDamage(damage, hitPoint, hitDirection);
+        playerAudioPlayer.PlayOneShot(playerHurtClip);
         healthSlider.value = currentHealth;
+    
+        StartCoroutine(CoChangePostProcess(0.5f));
     }
 
     public override void OnDie()
@@ -35,11 +47,26 @@ public class PlayerHealth : LivingEntity
         GetComponent<PlayerMovement>().enabled = false;
         GetComponent<PlayerShooter>().enabled = false;
         playerAnimator.SetTrigger("Die");
+        playerAudioPlayer.PlayOneShot(playerDeathClip);
     }
 
     private void RestartLevel()
     {
         GameManager.Instance.GameOver();
         Time.timeScale = 0f;
+    }
+
+    private IEnumerator CoChangePostProcess(float duration)
+    {
+        postProcessVolume.profile = hurtProfile;
+
+        float timer = 0f;
+        while (timer < duration)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        postProcessVolume.profile = normalProfile;
     }
 }
